32d8601c71644af0dd0947f1f4a9a34c74220c6d
Orange-Juice03 <alvaroe.glz03@gmail.com>
2024-10-26 14:41:31 -0500

    Upate app.py file

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ def seconds_since_epoch():
 
 @app.route("/")
 def seconds():
-    return seconds_since_epoch()
+    return str(seconds_since_epoch())
+    
+def minutes_since_epoch():
+    epoch = time.time() 
 
+
+    def hours_since_epoch():
+        epoch = time.time()
+        return int(epoch // 3600)
+
+    @app.route("/hours")
+    def hours():
+        return str(hours_since_epoch())
